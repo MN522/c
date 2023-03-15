21cd0aba831e0a90473ede6c2d6659916c692121
nmsoc <58423518+MN522@users.noreply.github.com>
2023-03-15 23:51:11 +0900

    ファイルにALDS1_4_Bを追加し10/10合格した

--- ALDS1_4_B_BinarySearch.c
+++ ALDS1_4_B_BinarySearch.c
@@ -1,58 +1,27 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
-#define MAX_S_SIZE 100000
+#define MAX_S_SIZE 100005
 #define MAX_T_SIXE 50000
 bool reBinarySearch(int* pArry, int Target, int LeftIndex, int RightIndex);
 
 int main(){
-    //Data読み込み（空白がある長さ決まっている数列の読み込み）
+    int n, q, t;
     int S[MAX_S_SIZE];
-    int n, q;
     int T[MAX_T_SIXE];
-    char SBuf[MAX_S_SIZE];
-    char TBuf[MAX_T_SIXE];
 
-    scanf("%d",&n);
-    int dt[128];
-    int i=0,k,f;
-    char *pSBuf=SBuf;
-    char *pTBuf=TBuf;
-    fgets(SBuf,MAX_S_SIZE*2,stdin);//前行の不要な改行を捨てる
-    fgets(SBuf,MAX_S_SIZE*2,stdin);//fgetsはscnafの入力が標準入力ではなく文字列配列になったVer
-    while(i < n){
-        f=sscanf(pSBuf,"%d",&k);
-        S[i] = k;
-        i++;
-        while(*pSBuf != ' '){
-            pSBuf++;
-            if (*pTBuf == '\n'){
-                break;
-                }
-            }
-        pSBuf++;
+    //Data読み込み（空白がある長さ決まっている数列の読み込み）
+    scanf("%ld",&n);
+    for(int i = 0 ; i < n; i++){
+        scanf("%ld",&S[i]);
     }
 
-    scanf("%d",&q);
-    i=0;
-    fgets(TBuf,MAX_S_SIZE*2,stdin);//前行の不要な改行を捨てる
-    fgets(TBuf,MAX_S_SIZE*2,stdin);
-    while(i < q){
-        f=sscanf(pTBuf,"%d",&k);
-        T[i] = k;
-        i++;
-        while(*pTBuf != ' '){
-            pTBuf++;
-            if (*pTBuf == '\n'){
-                break;
-                }
-            }
-        pTBuf++;
-        
+    scanf("%ld",&q);
+    for(int i = 0 ; i < q; i++){
+        scanf("%ld",&T[i]);
     }
 
     //2分探索
-    int Mid, MidIndex;
     int LeftIndex, RightIndex;
     int Target;
     int Cnt = 0;
