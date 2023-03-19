fc3719e80dc23666ad1709642491e912e3eb7676
nmsoc <58423518+MN522@users.noreply.github.com>
2023-03-19 12:59:18 +0900

    [完成]ポインタ教科書のp103の例ソースコード

--- List 2_5 tiny_printf.c
+++ List 2_5 tiny_printf.c
@@ -6,21 +6,27 @@ void tiny_printf(char *format, ...){
     int i;
     va_list ap; //可変長仮引数(...)を保持する型
 
-    va_start(ap, format);//formatの次以降をapに格納
+    va_start(ap, format);//formatの次以降のアドレスをポインタapに格納
     for(i = 0; format [i] !='\0'; i++){
         switch (format[i])
         {
         case 's':
-            printf("%s ",va_arg(ap, char*));
+            printf("%s ",va_arg(ap, char*));//apの示すアドレスを出力。apの示すアドレスはインクリメントされる
             break;
-        
+        case 'd':
+            printf("%d ",va_arg(ap, int));
+            break;
         default:
             break;
         }
     }
+    va_end(ap);
+    putchar('\n');
 }
 
+
+
 int main(void){
-    tiny_printf("s", "sesulr");
+    tiny_printf("sdd", "result",3,5);
     return 0;
 }
