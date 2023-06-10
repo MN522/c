5dd77bdd4c0bc7a55d28a909bdca1ff50cf7bf52
signal_pak <58423518+MN522@users.noreply.github.com>
2023-06-10 22:13:51 +0900

    Delete word_count_P246 directory

--- word_count_P246/dump_word.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "word_manage_p.h"
-
-void dump_word(FILE *fp){
-    int i;
-    for (i = 0; i<num_of_word;i++){
-        fprintf(fp, "%-20s%5d\n",word_arry[i].name, word_arry[i].count);
-    }
-}
